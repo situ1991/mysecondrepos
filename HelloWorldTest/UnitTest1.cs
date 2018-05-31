d96c1fd6d2807661bf0d73694376a3427e791bc9
Sitangshu Chand <git.situ@email.com>
2018-05-31 07:08:01 +0000

    This is first commit

---
+++ HelloWorldTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HelloWorld;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello,World!", Program.CreateMessage());
+        }
+    }
+}
